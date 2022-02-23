7b35faa9ede98d45b64f1c8b778c810f1a52fd88
Breanne Beutler <breannebeutler@unomaha.edu>
2022-02-22 21:53:27 -0600

    update navigation bar

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <div>
     <nav class="navbar navbar-dark bg-dark justify-content-between flex-nowrap flex-row">
       <div class="container">
-        <a class="navbar-brand float-left">Firebase Vue CRUD Example</a>
+        <a class="navbar-brand float-left">Kudos</a>
         <ul class="nav navbar-nav flex-row float-right">
           <li class="nav-item">
             <router-link class="nav-link pr-3" to="/">Add Task</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/list">View Tasks</router-link>
+            <router-link class="nav-link" to="/list">View Task List</router-link>
           </li>
         </ul>
       </div>
