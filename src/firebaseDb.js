535ff9ba7c21d810c79e06b1fb9d573f8b3cc2f6
Breanne Beutler <breannebeutler@unomaha.edu>
2022-03-20 17:19:32 -0500

    major test lol

--- src/firebaseDb.js
+++ src/firebaseDb.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCwY4qIRkW_3-8p6Q_-by5ETiYKfOTJACk",
-    authDomain: "kudosproject-668dc.firebaseapp.com",
-    projectId: "kudosproject-668dc",
-    storageBucket: "kudosproject-668dc.appspot.com",
-    messagingSenderId: "23453393552",
-    appId: "1:23453393552:web:0f0624b5eafa6d4953ae15"
+  apiKey: "AIzaSyDhTP6sNSs3c4n55V69tM1dH--pBgEjUtE",
+  authDomain: "soft-kudos.firebaseapp.com",
+  projectId: "soft-kudos",
+  storageBucket: "soft-kudos.appspot.com",
+  messagingSenderId: "272127081098",
+  appId: "1:272127081098:web:622bb52ed1aaf7c8f20b73"
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -1,29 +1,10 @@
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import firebase from 'firebase'
+import App from './App.vue'
+import store from './store'
+
 Vue.config.productionTip = false
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDhTP6sNSs3c4n55V69tM1dH--pBgEjUtE",
-  authDomain: "soft-kudos.firebaseapp.com",
-  projectId: "soft-kudos",
-  storageBucket: "soft-kudos.appspot.com",
-  messagingSenderId: "272127081098",
-  appId: "1:272127081098:web:622bb52ed1aaf7c8f20b73"
-};
-firebase.initializeApp(firebaseConfig);
-
-
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: {
-    App
-  },
-
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
